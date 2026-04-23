21f774426708f788fe54ef624806b5647d6bcb4a
achill-242 <achilleas.stavroulis@gmail.com>
2026-04-23 18:25:33 +0200

    Update dino.py

--- dino.py
+++ dino.py
@@ -1,22 +1,52 @@
 import pgzrun
-
+import time
 TITLE = "Dino Run"
 WIDTH = 500
 HEIGHT = 500
-
+Score = 0 
 bg = Actor("ground")
 ob = Actor("obs")
 d = Actor("dino")
+ob2 = Actor("box")
 d.pos = (50,295)
 bg.pos = (250,250)
-ob.pos = (250,281)
+ob.pos = (250,320)
+ob2.pos = (350,320)
+
 def draw():
-    screen.fill(color = "white")
+    global Score
+    screen.fill(color="white")
+    # screen.blit("ground",(0,250))
     bg.draw()
     d.draw()
     ob.draw()
+    ob2.draw()
+    screen.draw.text("Score: " + str(Score),color = "blue", topleft =(10,10))
 def update():
-    bg.x -= 2     
+    global Score
+    """bg.x -= 2     
     if bg.right < 0:
-        bg.left = WIDTH 
+        bg.left = WIDTH""" 
+    ob.x-=2     
+    if ob.right<0:
+        ob.left = WIDTH
+    ob2.x -= 2
+    if ob2.right < 0:
+         ob2.left = WIDTH 
+    d.y+=4
+    if d.y >= 295:
+        d.y = 295
+    if keyboard.space:
+        d.y-=5
+    if ob.x<90:     
+        if d.colliderect(ob):
+            Score -= 10
+            
+            ob.x=WIDTH
+    if ob2.x<90:     
+        if d.colliderect(ob2):
+            Score -= 10
+            
+            ob2.x=WIDTH
 pgzrun.go()
+    
